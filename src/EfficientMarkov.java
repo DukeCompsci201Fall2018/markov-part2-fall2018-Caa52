1718ec43664e0c6e0ca4eacc6e1b821f73257e6b
caa52 <collins.abanda@duke.edu>
2018-10-22 04:30:24 -0400

    markov 2 update

--- src/EfficientMarkov.java
+++ src/EfficientMarkov.java
@@ -2,43 +2,72 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
-
+/**
+ * 
+ * @author Collins Abanda
+ *Created EfficientMarkov as extension of BaseMarkov
+ *Initialized new map called myMap
+ */
 public class EfficientMarkov extends BaseMarkov{
 	Map<String, ArrayList<String>> myMap = new HashMap<String, ArrayList<String>>();
-
+	
+//created EJfficientMarkov constructor
+//myMap assigned to map of string and arraylist
 	public EfficientMarkov(int order) {
 		super(order);
 		myMap = new HashMap<String, ArrayList<String>>();
 	}
-	
+//set this to 3 as in BaseMarkov	
 	public EfficientMarkov() {
 		this(3);
 	}
-
+	//created setTraining constructor and called override
+	@Override
 	public void setTraining(String text) {
+		//Initialize text to variable myText
+		//initialized the values as myVal
+		//cleared map 
 		myText = text;
-		String myVal = " ";   
+		String myVal = "";   
 		myMap.clear();
-		for(int i=0; i <= myText.length() - myOrder ; i++){
-			String myKey = myText.substring(i, i+ myOrder);
+		//for loop loops through the text to extract map keys from text
+		for(int i=0; i <= myText.length() - myOrder + 1; i++){
+			//myText.substring gets characters from index i up to but not
+			//including index i+ myOrder
+			String myKey = myText.substring(i, i + myOrder);
+			//if key in map, set key value
+			ArrayList<String> next = new ArrayList<String>();
+			if (myMap.containsKey(myKey)) {
+				next = myMap.get(myKey);
+			}
+			//if key not in map, put key and assign an array list for values
 			if(! myMap.containsKey(myKey)) {
 				myMap.put(myKey, new ArrayList<String>());
 			}
-			myVal = Character.toString(myText.charAt(i + myOrder));
-			ArrayList<String> next = new ArrayList<String>();
-			next = myMap.get(myKey);
-			next.add(myVal);
-			myMap.put(myKey, next );
-			
+			//when the code gets to the last set of keys, return PSEUDO_EOS
 			if(i == myText.length() - myOrder) {
 				next.add(PSEUDO_EOS);
+				myMap.put(myKey, next);
+				break;
+			//break out of for loop in order to not have an out of bounds exception
 			}
+			else {
+				//initialize vals to this case otherwise
+				myVal = myText.substring(i + myOrder, i + myOrder + 1);
+				next.add(myVal);
+			}
+			//builds map with key and arraylist of values 
+			myMap.put(myKey, next);
 			
-			//myMap.put(myKey, myMap.get(myKey);
 		}
 
 	}
-	
+	/**
+	 * created the getFollows constructor
+	 * check to see if map contains key or not 
+	 * return null at the end 
+	 */
+	@Override
 	public ArrayList<String> getFollows(String key){
 		if (myMap.containsKey(key)) {
 			return myMap.get(key);
--- src/EfficientWordMarkov.java
+++ src/EfficientWordMarkov.java
@@ -3,6 +3,15 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
+/**
+ * 
+ * @author Collins Abanda
+ *@param
+ *created EfficientWordMarkov class extension
+ *initialized map as WordGram and ArrayList just as in BaseWordMarkov
+ *created the EfficientWordMarkov constructor to initialize new map of WordGram, ArrayList called myMap
+ *Set EfficentWordMarkov to 2
+ */
 public class EfficientWordMarkov extends BaseWordMarkov {
 	private Map<WordGram, ArrayList<String>> myMap;
 
@@ -21,41 +30,51 @@ public class EfficientWordMarkov extends BaseWordMarkov {
 	 * @param source
 	 * @param start
 	 * @param size
+	 * 
+	 * called override on setTraining
+	 * setTraining parameter is set as text
 	 */
 	@Override
 	public void setTraining(String text) {
+		//split white spaces between words of text 
 		myWords = text.split("\\s+");
+		//clear map
 		myMap.clear();
-		ArrayList<String> myList;
-
-		for (int i = 0; i < myWords.length - myOrder + 1; i++) {
-			myList = new ArrayList<String>();
-
+		//loop through the words in text
+		for(int i=0; i <= myWords.length - myOrder + 1; i++){
+			//Create new WordGram called Owen
 			WordGram Owen = new WordGram(myWords, i, myOrder);
-
-			String nextStr = " ";
-			if (i < myWords.length - myOrder) {
-				nextStr = myWords[i + myOrder];
-
-			}
-			if (i == myWords.length - myOrder) {
-				nextStr = PSEUDO_EOS;
-
-			}
-
-			if (!myMap.containsKey(Owen)) {
-				myList.add(nextStr);
-				myMap.put(Owen, myList);
-
-			}
-
-			else {
+			//create new string arraylist 
+			ArrayList<String> myList = new ArrayList<String>();
+			
+			//if key Owen in in map, assign Owen a value in map
+			if (myMap.containsKey(Owen)) {
 				myList = myMap.get(Owen);
-				myList.add(nextStr);
-				myMap.put(Owen, myList);
 			}
+			//if key Owen not in map, put Owen in map and assign Owen a 
+			//new string arrayList to hold its values
+			if(! myMap.containsKey(Owen)) {
+				myMap.put(Owen, new ArrayList<String>());
+			}
+			//if you get to the end of the text, return PSEUDO_EOS and break
+			if(i == myWords.length - myOrder) {
+				myList.add(PSEUDO_EOS);
+				myMap.put(Owen, myList);
+				break;
+			}
+			//else add these to values called myList
+			else {
+				myList.add(myWords[i + myOrder]);
+			}
+			//build map with key and values
+			myMap.put(Owen, myList);
 		}
 	}
+	
+	/**
+	 * Overide getFollows constructor
+	 * check weather or not Key is in new map
+	 */
 
 	@Override
 	public ArrayList<String> getFollows(WordGram key) {
